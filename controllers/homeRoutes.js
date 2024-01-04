b2e7e2aa304066f0a0214927cf051748dacbe14b
Chris Purcell <chrisp0000@gmail.com>
2024-01-03 21:23:13 -0800

    Added controllers for api and homeRoutes. Updated homeRoutes with get route to profile page

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Workouts } = require('../models');
+const auth = require('../utils/auth');
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const workoutData = await Workout.findAll({
+            include: [
+                {
+                    model: 'workout',
+                    attribute: ['time', 'distance', 'date_created']
+                }
+            ]
+        });
+
+        const workouts = workoutData.map((workout) => project.get({ plain: true }));
+
+        res.render('profile', {
+            workouts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
