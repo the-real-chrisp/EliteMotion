e1a330f15287fe51668de0022de8300a29160952
Chris Purcell <chrisp0000@gmail.com>
2024-01-10 19:08:53 -0800

    Update input to number and session save in sign up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -65,9 +65,13 @@ router.post('/signup', async (req, res) => {
             name: req.body.email,
             email: req.body.email,
             password: req.body.password
-        })
-        res.json(newUser)
-
+        });
+        // res.json(newUser)
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: 'You are now logged in!' });
+          });
     } catch (err) {
         res.status(400).json(err)
     }
